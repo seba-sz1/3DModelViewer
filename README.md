3e137ffa5157e6e91157d5882129896fbef69b24
Sebastian <sebastianszczy@gmail.com>
2023-10-09 16:01:49 +0200

    Add file .env and repair security in settings.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# 3DModelViewer
+# 3D Model Viewer API
+
+This is a Python-based API for rendering and viewing 3D models in a graphical window. The API is designed to be flexible and extensible, and can work with a variety of 3D file formats.
+
+## Requirements
+
+To use the 3D Model Viewer API, you will need:
+
+- Python 3.x installed on your system
+- A 3D file format parser library such as PyWavefront or PyMesh
+- A graphics library such as PyOpenGL or Pygame
+
+## Installation
+
+To install the API, you can either clone this repository or download the ZIP file and extract it to your local machine. Once you have the files, you can install any necessary dependencies by running:
+
+```
+pip install -r requirements.txt
+```
+
+## Usage
+
+To use the API, you will need to write code to load 3D models and render them in a window. Here is an example code snippet that loads an OBJ file and renders it using PyOpenGL:
--- model3DView/model3DView/settings.py
+++ model3DView/model3DView/settings.py
@@ -10,20 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv.load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$51*_447hy$j(zhdr-3z-(n5+wx6su96ba4b0%sur67c5e)l(!'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'modelView',
 ]
 
 MIDDLEWARE = [
