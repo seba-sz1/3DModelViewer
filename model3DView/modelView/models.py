806b63406f86e5e70ef302b6ccdf75e8c8b38417
Sebastian <sebastianszczy@gmail.com>
2023-10-11 14:21:29 +0200

    Repair model ThreeDModel and add first object to media.

--- model3DView/modelView/models.py
+++ model3DView/modelView/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class ThreeDModel(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    filePath = models.FilePathField(max_length=1000)
+    file = models.FileField(upload_to='files/')
+    description = models.TextField(max_length=500, blank=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name} | {self.description}"
